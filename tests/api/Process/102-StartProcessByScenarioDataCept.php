b8298de1020f41fcd87ed8f8c2bc2fa6badc3947
Sergey Boltonosov <minstel@yandex.ru>
2019-03-16 08:36:16 +0700

    Fixed api/Process/102-StartProcessByScenarioDataCept

--- tests/api/Process/102-StartProcessByScenarioDataCept.php
+++ tests/api/Process/102-StartProcessByScenarioDataCept.php
@@ -3,14 +3,15 @@
 $I = new ApiTester($scenario);
 $I->wantTo('start a process, passing scenario data');
 
-$I->am('organization');
+$I->signRequestAs('organization', 'POST', '/processes');
 
 $I->sendPOST('/processes', [
+    'id' => '823d1e54-9009-4745-8901-dd62ec46eaf2',
     'scenario' => ['id' => '2557288f-108e-4398-8d2d-7914ffd93150']
 ]);
 
 $I->seeResponseIsJson();
 $I->seeResponseCodeIs(200);
 
-$I->seeResponseJsonMatchesJsonPath('$.id');
-$I->seeResponseContainsJson(['scenario' => '2557288f-108e-4398-8d2d-7914ffd93150']);
+$I->seeResponseContainsJson(['id' => '823d1e54-9009-4745-8901-dd62ec46eaf2']);
+$I->seeResponseIsProcess('basic-user-and-system', 'no-user-identity');

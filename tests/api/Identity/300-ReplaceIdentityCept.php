0acf527efd48642bf0adeb55e3359ccdb02efb5f
Sergey Boltonosov <minstel@yandex.ru>
2019-03-17 06:44:00 +0700

    Single route for adding and updating identities

--- tests/api/Identity/300-ReplaceIdentityCept.php
+++ tests/api/Identity/300-ReplaceIdentityCept.php
@@ -1,12 +1,13 @@
 <?php
 
 $I = new ApiTester($scenario);
-$I->wantTo('update an identity');
+$I->wantTo('replace an identity');
 
 $id = '1237288f-8u6f-3edt-8d2d-4f4ffd938vk';
 
 $I->haveHttpHeader('Content-Type', 'application/json');
-$I->sendPOST('/identities/' . $id, [
+$I->sendPOST('/identities', [
+    'id' => $id,
     'node' => 'amqps://example.com',
     'signkeys' => [
         'user' => '57FWtEbXoMKXj71FT84hcvCxN5z1CztbZ8UYJ2J49Gcn',
@@ -24,7 +25,7 @@ $expected = [
         'user' => '57FWtEbXoMKXj71FT84hcvCxN5z1CztbZ8UYJ2J49Gcn',
         'system' => '2gYvvF9nyjaC5Qv3mUDFkbqXNEWDtoJxZKwnHEtGRDzP',
     ],
-    'encryptkey' => '9fSos8krst114LtaYGHQPjC3h1CQEHUQWEkYdbykrhHv',
+    'encryptkey' => null,
 ];
 
 $I->seeResponseContainsJson($expected);

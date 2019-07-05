79d65724cd7b9d9b99a697dce0e07761b40ce0e1
Sergey Boltonosov <minstel@yandex.ru>
2019-07-06 02:34:47 +0700

    Use another multipart parser

--- models/HttpRequestLog.php
+++ models/HttpRequestLog.php
@@ -1,8 +1,8 @@
-<?php
+<?php declare(strict_types=1);
 
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
-use h4cc\Multipart\Parser\MultipartParser;
+use Riverline\MultiPartParser\StreamedPart;
 
 /**
  * Class for logging single http request and it's response
@@ -78,30 +78,63 @@ class HttpRequestLog extends MongoDocument
 
                 return $values;
             case 'multipart/form-data': 
+                $body = $this->normalizeMultipartBody($body);
                 return $this->parseMultipartBody($body);
         }
 
         return $body;
-     }
+    }
+
+    /**
+     * Prepend headers to body
+     *
+     * @param string $body
+     * @return string
+     */
+    protected function normalizeMultipartBody(string $body): string
+    {
+        preg_match('|^--(\S+)\r?\n|', $body, $match);
+
+        $boundary = $match[1] ?? null;
+        if (!empty($boundary)) {
+            //At least this header is needed for parser to work
+            $body = "Content-Type: multipart/form-data; boundary=$boundary\n\n$body";
+        }
+
+        return $body;
+    }
 
     /**
      * Parse multipart body
      *
      * @param string $body
-     * @return array
+     * @return array|string
      */
-    protected function parseMultipartBody(string $body): array
+    protected function parseMultipartBody(string $body)
     {
-        preg_match('|^--(\S*)|', $body, $match);
+        $stream = fopen('php://temp', 'rw');
+        fwrite($stream, $body);
+        rewind($stream);
 
-        $boundary = $match[1] ?? null;
-        if (empty($boundary)) {
+        $document = new StreamedPart($stream);
+        if (!$document->isMultiPart()) {
             return $body;
         }
 
-        $parser = new MultipartParser();
-        $parser->setBoundary($boundary);
+        $result = [];
+        $parts = $document->getParts();
 
-        return $parser->parse($body);
+        foreach ($parts as $part) {
+            $name = $part->getName();
+            $value = $part->getBody();
+            $headers = $part->getHeaders();
+
+            $result[$name] = [
+                'value' => $value,
+                'headers' => $headers
+            ];
+        }
+
+        return $result;
      }
 }

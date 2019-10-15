4a16577fe490312a8e4c81fc4cf7fdfbb508be18
sven4ask <sven@legalthings.io>
2019-10-15 16:30:37 +0200

    Add support tester classes

---
+++ tests/_support/ApiTester.php
@@ -0,0 +1,31 @@
+<?php
+
+
+/**
+ * Inherited Methods
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
+ *
+ * @SuppressWarnings(PHPMD)
+*/
+class ApiTester extends \Codeception\Actor
+{
+    use _generated\ApiTesterActions;
+
+   /**
+    * Define custom actions here
+    */
+    public function am($role)
+    {
+        parent::am($role);
+
+        $this->amSignatureAuthenticatedAs($role);
+    }
+}
